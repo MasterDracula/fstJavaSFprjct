0e4ecde202ec046f8434db16245bd882de26d0ec
Name <IceClock@yandex.ru>
2024-09-14 19:03:07 +0300

    1) add al func, 2) create quit

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -5,40 +5,49 @@ public class MyCalculator {
         System.out.println("Приветствую в моем скромном калькуляторе! :)\nВведите пожалуйста два числа: ");
         Scanner input=new Scanner(System.in);
         char operator;
+        double fstnum = 0;
+        double scdnum = 0;
         if (input.hasNextInt()){
-            int fstnum=input.nextInt();
-            int scdnum=input.nextInt();
+            fstnum = input.nextInt();
+            scdnum = input.nextInt();
         }else{
             System.out.println("Я умею работать только с числами :( ");
         }
-        System.out.println("Выберите простую операцию (+,-,*,/) и подтвердите её или нажмите Q для выхода из программы");
+        System.out.println("+ для сложения");
+        System.out.println("- для вычитания");
+        System.out.println("* для умножения");
+        System.out.println("/ для деления");
+        System.out.println("Q для выхода");
+        System.out.println("Выберите из перичисленного: ");
         operator = input.next().charAt(0);
-        double result;
+        double result = 0;
         switch (operator){
             case '+':
                 result = fstnum + scdnum;
+                System.out.println("Результат операции: "+ result);
                 break;
             case '-':
                 result = fstnum - scdnum;
+                System.out.println("Результат операции: "+ result);
                 break;
             case '*':
                 result = fstnum * scdnum;
+                System.out.println("Результат операции: "+ result);
                 break;
             case '/':
                 if (scdnum == 0){
                     System.out.println("На ноль нельзя делить!");
-                    return;
                 }else{
-                    result = fstnum * scdnum;
-                    break;
+                    result = fstnum / scdnum;
+                    System.out.println("Результат операции: "+ result);
                 }
+                break;
             case 'q':
                 System.out.println("До свидания!");
                 break;
             default:
                 System.out.println("Я пока не знаю других операций с цислами :(");
         }
-        System.out.println("Рузльтат операции: "+ result);
-
+        System.out.println("Программа завершена");
     }
 }
